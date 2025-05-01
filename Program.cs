b8ed53b67cd965b5ded70e786275965709e3cb4d
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2025-05-01 06:26:45 -0300

    Valida objetos nulos

--- Program.cs
+++ Program.cs
@@ -17,17 +17,23 @@ public class Program
         string jsonString = File.ReadAllText(backupConfig);
         var jsonOptions = GetJsonOptions();
         var dataBackupConfig = JsonSerializer.Deserialize<BackupConfig>(jsonString, jsonOptions)!;
+        if (dataBackupConfig == null) throw new ArgumentException(nameof(DirectoryBackup));
 
         if (dataBackupConfig?.DestinationRoot?.Count <= 0 || dataBackupConfig?.DestinationRoot?.Count <= 0) { return; }
         var today = DateTime.Today;
-        string todayPath = Path.Combine(dataBackupConfig?.DirectoriesToBackup, today.ToString("dd.MM.yyyy"));
+        if (dataBackupConfig?.DirectoriesToBackup == null)
+        {
+            throw new InvalidOperationException("O caminho de backup não está configurado.");
+        }
+        string todayPath = Path.Combine(dataBackupConfig.DirectoriesToBackup, today.ToString("dd.MM.yyyy"));
 
         for (int i = 0; i < dataBackupConfig?.DestinationRoot?.Count; i++)
         {
             if (!Path.Exists(dataBackupConfig.DestinationRoot[i].SourcePath) &&
-                !Path.Exists(Path.Combine(dataBackupConfig.DirectoriesToBackup, dataBackupConfig?.DestinationRoot[i]?.DestinationFolder))) { return; }
+                !Path.Exists(Path.Combine(dataBackupConfig.DirectoriesToBackup, dataBackupConfig.DestinationRoot[i].DestinationFolder))) { return; }
             if (!Path.Exists(todayPath)) { Directory.CreateDirectory(todayPath); }
             if (!Path.Exists(todayPath)) { return; }
+            if (dataBackupConfig.DestinationRoot[i].SourcePath == null) { return; }
 
             for (global::System.Int32 j = 0; j < dataBackupConfig?.DestinationRoot[i]?.FileFormats?.Count; j++)
             {
